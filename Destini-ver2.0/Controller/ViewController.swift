4798ea2735ee3b73b4ba5859a066379435361ff7
Mohammad Agung <diditkazama@gmail.com>
2020-01-10 21:42:08 +0700

    buat struct story, update viewcontroller

--- Destini-ver2.0/Controller/ViewController.swift
+++ Destini-ver2.0/Controller/ViewController.swift
@@ -14,9 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story = Story(title: "hai", choice1: "1", choice2: "1")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
